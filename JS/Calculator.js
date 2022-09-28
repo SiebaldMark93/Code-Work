4dcfec54398a74b583b8dada80d788953be7630a
Mark Siebald <siebald.mark93@gmail.com>
2022-09-27 19:10:05 -0500

    feat. working on js

---
+++ JS/Calculator.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const allClearButtons = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand-operand]')
+
+
